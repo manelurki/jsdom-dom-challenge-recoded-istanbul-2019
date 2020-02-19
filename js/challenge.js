aae161b9fb759458da725826df4c7bc659c559ce
manel turki <menelturki@gmail.com>
2020-02-19 19:55:57 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -9,3 +9,7 @@ document.getElementById("minus").addEventListener("click",decrease);
 document.getElementById("plus").addEventListener("click",increase);
 document.getElementById("heart").addEventListener("click",heart);
 document.getElementById("pause").addEventListener("click",pause);
+document.getElementById("submit").addEventListener("click",addComment);
+function decrease(){
+  let result=parseInt(num.innerText)
+}
